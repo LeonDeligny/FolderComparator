b5a9dd94a1bed17b2f52593456dfc5e9a2db8972
LeonDeligny <leon.deligny@icloud.com>
2025-10-13 16:43:28 +0200

    mroe robust python version in get argumetns

--- src/main.py
+++ src/main.py
@@ -225,8 +225,21 @@ if __name__ == "__main__":
     # Define the minimum required Python version (e.g., 3.8 for standard fnmatch.translate behavior)
     MIN_PYTHON_VERSION = (3, 8)
     if sys.version_info < MIN_PYTHON_VERSION:
-        print(f"Error: This script requires Python {MIN_PYTHON_VERSION[0]}.{MIN_PYTHON_VERSION[1]} or higher.", file=sys.stderr)
-        print(f"You are currently using Python {sys.version_info.major}.{sys.version_info.minor}.", file=sys.stderr)
+        print(
+            "Error: This script requires Python",
+            MIN_PYTHON_VERSION[0],
+            ".",
+            MIN_PYTHON_VERSION[1],
+            " or higher.",
+            file=sys.stderr,
+        )
+        print(
+            "You are currently using Python ",
+            sys.version_info.major,
+            ".",
+            sys.version_info.minor,
+            file=sys.stderr,
+        )
         print("Please use `python3.11` if available, or upgrade your Python version.", file=sys.stderr)
         sys.exit(1)
 
